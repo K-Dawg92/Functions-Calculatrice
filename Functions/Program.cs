556cac5f535a99628921afd0f96ed72ddb2df10e
K-Dawg92 <kevin.osei.yaw@gmail.com>
2024-06-14 17:26:13 +0200

    Ajoutez des fichiers projet.

---
+++ Functions/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+static double Add(double numberx, double numbery)
+{
+    double result1 = numberx + numbery;
+    return result1; 
+}
+static double substract(double numberx, double numbery)
+{
+    double result1 = numberx - numbery;
+    return result1;
+}
+
+static double multiply(double numberx, double numbery)
+{
+    double result1 = numberx * numbery;
+    return result1;
+}
+static double modulo(double numberx, double numbery)
+
+{
+    double result1 = (numberx % numbery);
+    return result1;
+
+}
+
+static double divide(double numberx, double numbery)
+{ 
+double result1 = numberx / numbery;
+    return result1;
+}
+
+Console.WriteLine("Please enter a number with a decimal, young Ronin");
+
+double answer1 = Convert.ToDouble(Console.ReadLine().Replace('.',','));
+
+Console.WriteLine("enter another number with a decimal");
+
+double answer2 = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+
+
+
+Console.WriteLine("enter either +, -,*, / or %");
+
+string Operator = Console.ReadLine();
+
+switch (Operator)
+{
+    case "+":
+      
+        Console.WriteLine(Add(answer1, answer2));
+        break;
+
+    case "-":
+        
+        Console.WriteLine(substract(answer1, answer2));
+        break;
+
+    case "*":
+        
+        Console.WriteLine(multiply(answer1, answer2));
+        break;
+    case "/":
+        
+        Console.WriteLine(divide(answer1, answer2));
+        break;
+    case "%":
+        modulo(answer1, answer2);
+        Console.WriteLine(modulo(answer1, answer2));
+        break;
+
+}
+
